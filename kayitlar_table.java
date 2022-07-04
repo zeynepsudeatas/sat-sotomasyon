1a775bacd7d7271f2f1f3540f2e0b5123194776b
zeynepsudeatas <101126542+zeynepsudeatas@users.noreply.github.com>
2022-07-04 19:50:42 +0300

    Create kayitlar_table.java

---
+++ kayitlar_table.java
@@ -0,0 +1,44 @@
+package application;
+
+import javafx.collections.ObservableList;
+
+public class kayitlar_table {
+
+	public static ObservableList<Kayitlar> getItems() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public static ObservableList<Kayitlar> getSelectedItems() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public static ObservableList<Kayitlar> getSelectionModel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+	public static void setItems(ObservableList<Kayitlar> veriler) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static ObservableList<kayitlar_urun> getItems1() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public static ObservableList<kayitlar_urun> getSelectedItems1() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public static ObservableList<kayitlar_urun> getSelectionModel1() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public static ObservableList<kayitlar_urun> getselectedındex(){
+		return null;
+	}
+	
+
+}
